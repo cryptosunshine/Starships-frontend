8eda7e1a43ae1f6cffef824d1f21e2d5d5a30800
shine <shine@shinedeMacBook-Pro.local>
2022-12-23 11:54:28 +0800

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
-export const contractAddress = "0x193853c27916358eaae9e42644d292dc7ab0a47d"; // Immutable
-export const globalMetaData = "0x9d53f6f9f22f61adadc5ff5b70125a7d2387f736"; // Shared
+export const contractAddress = "0x0d08d0d46ac6f4934407a45766766102eeac2e14"; // Immutable
+export const globalMetaData = "0xa1c22c0b6115e512cb4002912125dc679f230ae5"; // Shared
 export const x0 = "0x0000000000000000000000000000000000000000";
